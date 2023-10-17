1f1b80caa03e1db9700242b30eaa59eb9f0c0621
Mohosin212601~ <mohosin2126@gmail.com>
2023-10-18 03:55:32 +0800

    added addcoffee and updatecoffee components and path

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,22 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import App from './App';
+import AddCoffee from './Components/AddCoffee';
+import UpdateCoffee from './Components/UpdateCoffee';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App></App>,
   },
+  {
+path:"/addCoffee",
+element:<AddCoffee></AddCoffee>,
+  },
+  {
+    path:"/updateCoffee",
+    element:<UpdateCoffee></UpdateCoffee>,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
