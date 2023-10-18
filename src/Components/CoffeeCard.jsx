e3d3c1428d220fba1983a095c45b7965dfa76608
Mohosin212601~ <mohosin2126@gmail.com>
2023-10-18 18:39:37 +0800

    added coffee card

--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
 
 
 const CoffeeCard = () => {
     return (
-        <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+            <figure><img src=""alt="Movie" /></figure>
+            <div className="flex justify-between w-full pr-4">
             
+                <div className="card-actions justify-end">
+                    <div className="btn-group btn-group-vertical space-y-4">
+                        <button className="btn">View</button>
+                        <Link to={`updateCoffee/id`}>
+                        <button className="btn">Edit</button>
+                        </Link>
+                        <button
+                            
+                            className="btn bg-orange-500">X</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
